a6cb157d92ba68d2554ba77c6b684b2aa6707a7a
Natalja <tasia@inbox.lt>
2023-06-19 21:34:11 +0300

    js modify

---
+++ js/calculiator.js
@@ -0,0 +1,24 @@
+const formCalc = document.getElementsByTagName('form')
+const rezult = document.getElementById('rezultatas')
+/*const number1 = 
+const number2 = 
+const number3 = 
+const number4 = 
+const number5 = 
+const number6 = 
+const number7 = 
+const number8 = 
+const number9 = 
+const number0 = 
+const symbolC = 
+const symbol1 = 
+const symbol2 = 
+const symbol3 = 
+const symbol4 = 
+const symbol5 = 
+const symbol6 = 
+const symbol7 = 
+const symbol8 = */
+
+console.log(formCalc);
+console.log(rezult);

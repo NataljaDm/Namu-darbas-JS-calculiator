d2eea7a9d0df7eed82c390486eee85775b7707e3
Natalja <tasia@inbox.lt>
2023-06-20 12:14:12 +0300

    number modify

--- js/calculiator.js
+++ js/calculiator.js
@@ -10,11 +10,7 @@ const sixDom = calcDom.querySelector(`.but6`);
 const sevenDom = calcDom.querySelector(`.but7`);
 const eightDom = calcDom.querySelector(`.but8`);
 const nineDom = calcDom.querySelector(`.but9`);
-/*const multiDom = document.getElementsByClassName(`.butx`);
-const fourDom = document.getElementsByClassName(`.but4`);
-const fifeDom = document.getElementsByClassName(`.but5`);
-const sixDom = document.getElementsByClassName(`.but6`);
-const minusDom = document.getElementsByClassName(`.but-`);*/
+
 
 let counter = 0;
 
@@ -70,38 +66,3 @@ nineDom.addEventListener('click', (event) => {
      counter =9;
      rezultDom.textContent = counter;
  });
-
-
-
-
-
-
-
-/*console.log(calcDom);*/
-
-
-/*console.log(symbolDom);
-console.log(procentDom);
-console.log(divDom);
-console.log(sevenDom);
-console.log(eightDom);*/
-console.log(nineDom);
-
-
-
-/*console.log(number6);
-console.log(number7);
-console.log(number8);
-/*console.log(number9);*/
-
-/*const number9 = calcDom.querySelector('button:nth-child(9)');
-const number0 = calcDom.getElementsByClassName('button2');*/
-/*const symbolC = calcDom.querySelector('button:nth-child(10)');
-const symbol1 = calcDom.querySelector('button:nth-child(11)');
-const symbol2 = calcDom.querySelector('button:nth-child(12)');
-const symbol3 = calcDom.querySelector('button:nth-child(13)');
-const symbol4 = calcDom.querySelector('button:nth-child(14)');
-const symbol5 = calcDom.querySelector('button:nth-child(15)');
-const symbol6 = calcDom.querySelector('button:nth-child(16)');
-const symbol7 = calcDom.querySelector('button:nth-child(17)');
-const symbol8 = calcDom.querySelector('button:nth-child(18)');*/

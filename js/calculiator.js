e83428961f1093ddee56913650dfea8e5cb39543
Natalja <tasia@inbox.lt>
2023-06-20 11:38:02 +0300

    calc

--- js/calculiator.js
+++ js/calculiator.js
@@ -1,24 +1,49 @@
-const formCalc = document.getElementsByTagName('form')
-const rezult = document.getElementById('rezultatas')
-/*const number1 = 
-const number2 = 
-const number3 = 
-const number4 = 
-const number5 = 
-const number6 = 
-const number7 = 
-const number8 = 
-const number9 = 
-const number0 = 
-const symbolC = 
-const symbol1 = 
-const symbol2 = 
-const symbol3 = 
-const symbol4 = 
-const symbol5 = 
-const symbol6 = 
-const symbol7 = 
-const symbol8 = */
+const calcDom = document.querySelector('form');
+const rezultDom = document.getElementById('rezultatas');
 
-console.log(formCalc);
-console.log(rezult);
+const cDom = calcDom.querySelector('button');
+const symbolDom = calcDom.querySelector('button:nth-child(2)');
+const procentDom = calcDom.querySelector('button:nth-child(3)');
+const divDom = calcDom.querySelector('button:nth-child(4)');
+const sevenDom = document.getElementsByClassName(`but7`);
+const eightDom = document.getElementsByClassName(`but8`);
+const nineDom = document.getElementsByClassName(`.but9`);
+const multiDom = document.getElementsByClassName(`butx`);
+const fourDom = document.getElementsByClassName(`but4`);
+const fifeDom = document.getElementsByClassName(`but5`);
+const sixDom = document.getElementsByClassName(`but6`);
+const minusDom = document.getElementsByClassName(`but-`);
+
+let counter = '';
+
+
+nineDOM.addEventListener('click', (event) => {
+     event.preventDefault();
+     rezultDOM.textContent = ++counter;
+ });
+   
+console.log(calcDom);
+console.log(rezultDom);
+
+console.log(symbolDom);
+console.log(procentDom);
+console.log(divDom);
+console.log(sevenDom);
+console.log(eightDom);
+console.log(nineDom);
+/*console.log(number6);
+console.log(number7);
+console.log(number8);
+/*console.log(number9);*/
+
+/*const number9 = calcDom.querySelector('button:nth-child(9)');
+const number0 = calcDom.getElementsByClassName('button2');*/
+/*const symbolC = calcDom.querySelector('button:nth-child(10)');
+const symbol1 = calcDom.querySelector('button:nth-child(11)');
+const symbol2 = calcDom.querySelector('button:nth-child(12)');
+const symbol3 = calcDom.querySelector('button:nth-child(13)');
+const symbol4 = calcDom.querySelector('button:nth-child(14)');
+const symbol5 = calcDom.querySelector('button:nth-child(15)');
+const symbol6 = calcDom.querySelector('button:nth-child(16)');
+const symbol7 = calcDom.querySelector('button:nth-child(17)');
+const symbol8 = calcDom.querySelector('button:nth-child(18)');*/

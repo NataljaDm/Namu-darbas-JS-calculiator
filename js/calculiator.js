893f9ad92beffaf507ee11f6d45dd8c652e6829c
Natalja <tasia@inbox.lt>
2023-06-20 12:13:18 +0300

    numbers

--- js/calculiator.js
+++ js/calculiator.js
@@ -1,36 +1,94 @@
 const calcDom = document.querySelector('form');
 const rezultDom = document.getElementById('rezultatas');
 
-const cDom = calcDom.querySelector('button');
-const symbolDom = calcDom.querySelector('button:nth-child(2)');
-const procentDom = calcDom.querySelector('button:nth-child(3)');
-const divDom = calcDom.querySelector('button:nth-child(4)');
-const sevenDom = document.getElementsByClassName(`but7`);
-const eightDom = document.getElementsByClassName(`but8`);
-const nineDom = document.getElementsByClassName(`.but9`);
-const multiDom = document.getElementsByClassName(`butx`);
-const fourDom = document.getElementsByClassName(`but4`);
-const fifeDom = document.getElementsByClassName(`but5`);
-const sixDom = document.getElementsByClassName(`but6`);
-const minusDom = document.getElementsByClassName(`but-`);
+const oneDom = calcDom.querySelector(`.but1`);
+const twoDom = calcDom.querySelector(`.but2`);
+const threeDom = calcDom.querySelector(`.but3`);
+const fourDom = calcDom.querySelector(`.but4`);
+const fifeDom = calcDom.querySelector(`.but5`);
+const sixDom = calcDom.querySelector(`.but6`);
+const sevenDom = calcDom.querySelector(`.but7`);
+const eightDom = calcDom.querySelector(`.but8`);
+const nineDom = calcDom.querySelector(`.but9`);
+/*const multiDom = document.getElementsByClassName(`.butx`);
+const fourDom = document.getElementsByClassName(`.but4`);
+const fifeDom = document.getElementsByClassName(`.but5`);
+const sixDom = document.getElementsByClassName(`.but6`);
+const minusDom = document.getElementsByClassName(`.but-`);*/
 
-let counter = '';
+let counter = 0;
 
+oneDom.addEventListener('click', (event) => {
+    event.preventDefault();
+    counter =1;
+    rezultDom.textContent = counter;
+});
 
-nineDOM.addEventListener('click', (event) => {
+twoDom.addEventListener('click', (event) => {
+    event.preventDefault();
+    counter =2;
+    rezultDom.textContent = counter;
+});
+
+threeDom.addEventListener('click', (event) => {
      event.preventDefault();
-     rezultDOM.textContent = ++counter;
- });
-   
-console.log(calcDom);
-console.log(rezultDom);
+     counter =3;
+     rezultDom.textContent = counter;
+});
 
-console.log(symbolDom);
+fourDom.addEventListener('click', (event) => {
+    event.preventDefault();
+    counter =4;
+    rezultDom.textContent = counter;
+});
+
+fifeDom.addEventListener('click', (event) => {
+    event.preventDefault();
+    counter =5;
+    rezultDom.textContent = counter;
+});
+
+sixDom.addEventListener('click', (event) => {
+     event.preventDefault();
+     counter =6;
+     rezultDom.textContent = counter;
+ });
+sevenDom.addEventListener('click', (event) => {
+    event.preventDefault();
+    counter =7;
+    rezultDom.textContent = counter;
+});
+
+eightDom.addEventListener('click', (event) => {
+    event.preventDefault();
+    counter =8;
+    rezultDom.textContent = counter;
+});
+
+nineDom.addEventListener('click', (event) => {
+     event.preventDefault();
+     counter =9;
+     rezultDom.textContent = counter;
+ });
+
+
+
+
+
+
+
+/*console.log(calcDom);*/
+
+
+/*console.log(symbolDom);
 console.log(procentDom);
 console.log(divDom);
 console.log(sevenDom);
-console.log(eightDom);
+console.log(eightDom);*/
 console.log(nineDom);
+
+
+
 /*console.log(number6);
 console.log(number7);
 console.log(number8);
